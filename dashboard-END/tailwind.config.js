9bea66909504e4ed5c4ce7bbbc0f9d65b91720eb
Mohamadap <121693482+Mohamadap@users.noreply.github.com>
2024-02-20 11:26:03 -0800

    Add files via upload
    Run the site with a live server

---
+++ dashboard-END/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode : 'class',
+  content: [
+    './src/**/*.{html,js}'
+  ],
+  theme: {
+    extend: {
+      fontFamily : {
+        vazir : ['Vazir'] 
+      }
+    },
+  },
+  plugins: [],
+}
